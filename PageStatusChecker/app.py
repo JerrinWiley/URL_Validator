1b33eee4567e62f9a2111c757b37285998508cbd
Jerrin Wiley <jerrin@tamu.edu>
2024-06-13 18:13:10 -0500

    Still WIP, working on making column dropdown update with new chosen file

--- PageStatusChecker/app.py
+++ PageStatusChecker/app.py
@@ -4,6 +4,10 @@ from PIL import Image as Imag
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
+import requests
+from datetime import datetime
+import pandas as pd
+from tqdm.notebook import tqdm
 
 import utils as U
 
@@ -11,21 +15,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # Function for opening the file explorer window
-        def browseFiles(label_file_explorer):
-            filename = filedialog.askopenfilename(initialdir = "/",
-                                                title = "Select a File",
-                                                filetypes = (("XLSX files",
-                                                                "*.xlsx*"),
-                                                                ("CSV files",
-                                                                "*.CSV*"),
-                                                            ("all files",
-                                                                "*.*")))
-            filename_brief = os.path.basename(filename)
-            # Change label contents
-            label_file_explorer.configure(text="File Selected: "+filename_brief)
-            file_labels = (filename_brief, label_file_explorer)
-            return(file_labels)
+        
 
         ###  App settings
         self.title('Page Status Web Crawler')
@@ -34,7 +24,7 @@ class App(tk.Tk):
         self.resizable(True, True)
         
         ### Set Icon
-        ico = Imag.open('JerrinLogo__256.jpg')
+        ico = Imag.open('PageStatusChecker/JerrinLogo__256.png')
         photo = ImageTk.PhotoImage(ico)
         self.iconphoto(False,photo)
         
@@ -63,22 +53,6 @@ class App(tk.Tk):
             self.configure(menu=menubar)
             frame.tkraise()
 
-        # # Create a File Explorer label
-        # label_file_explorer = tk.Label(self,
-        #                                text = "Select file with URL list",
-        #                                width = 100, height = 4,
-        #                                fg = "blue") 
-        
-              
-        # button_explore = tk.Button(self,
-        #                            text = "Browse Files",
-        #                            command = lambda:browseFiles(label_file_explorer)[1]) 
-        # button_exit = tk.Button(self,
-        #                         text = "Exit",
-        #                         command = exit)
-        # label_file_explorer.grid(column = 1, row = 1)
-        # button_explore.grid(column = 1, row = 2)
-        # button_exit.grid(column = 1,row = 3)
 
 #------------------------------------------------------ HOME PAGE FRAME / CONTAINER -------------------------------------------------------
 
@@ -86,10 +60,10 @@ class HomePage(tk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        label = tk.Label(self, text="Instructions", font=('Times', '20'))
+        label = tk.Label(self, text="Instructions", font=('Tahoma', '20'))
         label.pack(pady=0,padx=0)
-        T = Text(self, height=15, width=50)
-        textblock = """This program was created to take a list of URLs in XLSX or CSV format and individually ping each URL to determine the response code of the url and, if redirected, the final redirected URL and its respective response code."""
+        T = Text(self, font="Tahoma", padx=10, pady=10, height=10, width=50, wrap="word")
+        textblock = """This program was created to take a list of URLs in XLSX or CSV format and individually ping each URL to determine the response code of the url and, if redirected, the final redirected URL and its respective response code.\n\nTo begin, select "New Project" from the file menu.\n\nYour column headers must be in the top row. A blank first row will cause errors"""
 
         T.pack()
         T.insert(tk.END, textblock)
@@ -106,11 +80,11 @@ class HomePage(tk.Frame):
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=parent.quit)
 
-        ## processing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
+        # ## processing menu
+        # processing_menu = Menu(menubar, tearoff=0)
+        # menubar.add_cascade(label="Validation", menu=processing_menu)
+        # processing_menu.add_command(label="validate")
+        # processing_menu.add_separator()
 
         ## help menu
         help_menu = Menu(menubar, tearoff=0)
@@ -123,12 +97,89 @@ class HomePage(tk.Frame):
 #------------------------------------------------------ VALIDATION PAGE FRAME / CONTAINER -------------------------------------------------------
 
 class Validation(tk.Frame):
+    
     def __init__(self, parent, container):
+        filename = "Unknown"
         super().__init__(container)
 
-        label = tk.Label(self, text="Validation Page", font=('Times', '20'))
+        label = tk.Label(self, text="URL Validator", font=('Tahoma', '20'))
         label.pack(pady=0,padx=0)
-    
+
+        # Validator Function
+        def validator(sheetDir, valButtonState):
+            urls_raw = pd.read_excel(sheetDir)
+            try:
+                colNames = urls_raw.columns
+                for col in colNames:
+                    print(col)
+                options = colNames
+                clicked = tk.StringVar()
+                clicked.set(colNames[0])
+
+                drop = OptionMenu(self, clicked, *options)
+                if valButtonState.get() == False:
+                    drop.pack()
+                    valButtonState.set(True)
+                column_button = Button(self, text = "Check row count", command = show).pack
+
+                def show():
+                    label.config(text = clicked.get())
+            except:
+                print("Error")
+            return
+
+        # Function for opening the file explorer window
+        def browseFiles(label_file_explorer, buttonCheckState, sheetFileDir, valButtonState):
+            filename = filedialog.askopenfilename(initialdir = "/",
+                                                title = "Select a File",
+                                                filetypes = (("XLSX files",
+                                                                "*.xlsx*"),
+                                                                ("CSV files",
+                                                                "*.CSV*"),
+                                                            ("all files",
+                                                                "*.*")))
+            filename_brief = os.path.basename(filename)
+            sheetFileDir.set(filename)
+
+            # Change label contents
+            label_file_explorer.configure(text="File Selected: "+filename_brief)
+            file_labels = (filename_brief, label_file_explorer)
+
+            # Change button states
+            explore_button_text.set("Choose Different File")
+            if buttonCheckState.get()==False:
+                button_Check = tk.Button(self,
+                            text = "Check File Contents",
+                            command= lambda: validator(sheetFileDir.get(),valButtonState))
+                button_Check.pack(pady=10)
+                buttonCheckState.set(True)
+
+            return
+        
+    # Create a File Explorer label
+        label_file_explorer = tk.Label(self,
+                                       text = "Select file with URL list",
+                                       width = 100, height = 4,
+                                       fg = "blue") 
+        
+        # Set variables
+        sheetFileDir = tk.StringVar()
+        explore_button_text = tk.StringVar()
+        buttonCheckState = tk.BooleanVar()
+        buttonCheckState.set(False)
+        valButtonState = tk.BooleanVar()
+        valButtonState.set(False)
+
+        button_explore = tk.Button(self,
+                                   textvariable = explore_button_text,
+                                   command = lambda: browseFiles(label_file_explorer, buttonCheckState, sheetFileDir, valButtonState))
+        explore_button_text.set("Browse Files")
+        
+
+        label_file_explorer.pack()
+        button_explore.pack()
+
+
     def create_menubar(self, parent):
         menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
 
@@ -140,11 +191,11 @@ class Validation(tk.Frame):
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=parent.quit)
 
-        ## processing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
+        # ## processing menu
+        # processing_menu = Menu(menubar, tearoff=0)
+        # menubar.add_cascade(label="Validation", menu=processing_menu)
+        # processing_menu.add_command(label="validate")
+        # processing_menu.add_separator()
 
         ## help menu
         help_menu = Menu(menubar, tearoff=0)
